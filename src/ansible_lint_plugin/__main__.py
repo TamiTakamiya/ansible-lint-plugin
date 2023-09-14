f8335c89234f7712e012943d154f1767f255b578
Tami Takamiya <ttakamiy@redhat.com>
2023-09-14 11:01:33 -0400

    Add .gitignore, __main__.py and requirements.txt

---
+++ src/ansible_lint_plugin/__main__.py
@@ -0,0 +1,15 @@
+"""Execute main routine."""
+import logging
+
+
+_logger = logging.getLogger()
+
+
+def main() -> None:
+    """Execute main routine."""
+    _logger.info("Ansible Lint Plugin")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    main()
